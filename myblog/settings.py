ce6efff58c05cf696daf27e906fb28c309d9a240
Suren <ananian.suren96@gmail.com>
2025-02-24 20:37:02 +0400

    Update css

--- myblog/settings.py
+++ myblog/settings.py
@@ -121,7 +121,6 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 # Путь для хранения медиафайлов
